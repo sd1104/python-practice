b6fb1096455a838ea7ef75eeef35529feea82d45
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-22 12:00:05 +0900

    dict

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -3,3 +3,16 @@ fruits.append('melon')
 fruits.insert(0, 'peach')
 fruits.insert(5, 'lemon')
 del fruits[5]
+
+boys = {
+   'John': {
+     'age': 20,
+     'hobby': 'fishing',
+     'dream': 'pilot'
+   },
+   'Mike': {
+     'age': 21,
+     'hobby': 'singing',
+     'dream': 'driver'
+   }
+}
