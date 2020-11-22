130ff0763cbfb853f474c388acaa27d0daf570f3
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-22 13:58:06 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -1,27 +1,33 @@
-fruits = ['apple', 'orange', 'grape']
-fruits.append('melon')
-fruits.insert(0, 'peach')
-fruits.insert(5, 'lemon')
-del fruits[5]
+# fruits = ['apple', 'orange', 'grape']
+# fruits.append('melon')
+# fruits.insert(0, 'peach')
+# fruits.insert(5, 'lemon')
+# del fruits[5]
 
 
-boys = {
-   'John': {
-     'age': 20,
-     'hobby': 'fishing',
-     'dream': 'pilot'
-   },
-   'Mike': {
-     'age': 21,
-     'hobby': 'singing',
-     'dream': 'driver'
-   }
-}
+# boys = {
+#    'John': {
+#      'age': 20,
+#      'hobby': 'fishing',
+#      'dream': 'pilot'
+#    },
+#    'Mike': {
+#      'age': 21,
+#      'hobby': 'singing',
+#      'dream': 'driver'
+#    }
+# }
 
 
-for index, value in enumerate(fruits):
-  print(index, value)
+# for index, value in enumerate(fruits):
+#   print(index, value)
 
-for i in boys:
-  for e in boys[i]:
-    print(i, e, boys[i][e])
+# for i in boys:
+#   for e in boys[i]:
+#     print(i, e, boys[i][e])
+
+
+num = 0
+for i in range(10):
+  num = (i+1) + (i+2)
+print(num)
