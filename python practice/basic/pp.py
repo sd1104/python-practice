86393f1b43733f0346d96ad07c708be7a429ce99
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-28 21:00:36 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -4,4 +4,13 @@ fruits.append('lemon')
 fruits.insert(0, 'pine')
 fruits.extend(['strawberry', 'melon'])
 
-print(fruits)
+boys = {
+  'john': {
+    'age': 20,
+    'hobby': 'fishing'
+  },
+  'mike': {
+    'age': 21,
+    'hobby': 'singing'
+  }
+}
