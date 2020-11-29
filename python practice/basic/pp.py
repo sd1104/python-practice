6b6d9f9184043311e0117237bbc928d030c25d7f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-29 22:45:31 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -14,4 +14,14 @@ boys = {
   }
 }
 
-print(boys['john']['age'])
+for fruit in fruits:
+  print(fruit)
+
+for key1 in boys:
+  for key2 in boys[key1]:
+    print(key1, key2, boys[key1][key2])
+
+num = 0
+for i in range(10):
+  num += i+1
+print(num)
