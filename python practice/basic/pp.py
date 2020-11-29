d51ce8f22276cd94c00d1462165446e8b7ece533
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-29 22:53:35 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -22,3 +22,14 @@ for index, value in enumerate(fruits):
 for i in boys:
   for f in boys[i]:
     print(i, f, boys[i][f])
+
+def make_plus():
+  i = 0
+  while i < 10:
+    numA = random.randint(0,10)
+    numB = random.randint(0,10)
+    plus = numA + numB
+    formula = '{} + {}'
+    formula1 = formula.format(numA, numB)
+    print(formula1)
+    i += 1
