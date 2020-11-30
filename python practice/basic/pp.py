2473ca6625536f7bd3dadb250a03c13d20bc395c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-30 21:06:49 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -14,5 +14,9 @@ boys = {
   }
 }
 
-for fruit in fruits:
-  print(fruit)
+# for fruit in fruits:
+#   print(fruit)
+
+for key1 in boys:
+  for key2 in boys[key1]:
+    print(key1, key2, boys[key1][key2])
