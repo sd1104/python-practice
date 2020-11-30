cbf5b9d9f26322931d79daf465ad2608108841a5
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-30 21:29:58 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -17,6 +17,29 @@ boys = {
 # for fruit in fruits:
 #   print(fruit)
 
-for key1 in boys:
-  for key2 in boys[key1]:
-    print(key1, key2, boys[key1][key2])
+# for key1 in boys:
+#   for key2 in boys[key1]:
+#     print(key1, key2, boys[key1][key2])
+
+import random as rand
+
+# def make_array():
+#   num = []
+#   for i in range(1000):
+#     num.append(rand.randint(1,100)
+#   return num
+
+# def array_count(x):
+#   n = x.count(100)
+#   print(n)
+
+def make_rand():
+  n = []
+  for i in range(1000):
+    n.append(rand.randint(1,10))
+  return n
+
+def array_count(x):
+  print(x.count(10))
+
+array_count(make_rand())
