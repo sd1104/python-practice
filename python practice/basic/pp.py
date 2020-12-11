bd30aa55c5d0e02b0c91bd5ca1186a204536d0f4
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-12-11 22:17:45 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -1,7 +1,7 @@
 
 fruits = ['apple', 'orange', 'grape']
 
-print(fruits)
+# print(fruits)
 
 boys = {
   'john': {
@@ -17,6 +17,11 @@ boys = {
 # for fruit in fruits:
 #   print(fruit)
 
-for key1 in boys:
-  for key2 in boys[key1]:
-    print(key1, key2, boys[key1][key2])
+# for key1 in boys:
+#   for key2 in boys[key1]:
+#     print(key1, key2, boys[key1][key2])
+
+num = 0
+for i in range(10):
+  num = (i+2) + (i+1)
+print(num)
