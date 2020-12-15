7d531169bd8cc161c0e6615543ac3f4944b9c1f1
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-12-15 16:32:29 +0900

    Update pp.py

--- python practice/basic/pp.py
+++ python practice/basic/pp.py
@@ -1,3 +1,14 @@
 fruits = ['apple', 'orange', 'grape']
 
-print(fruits)
+boys = {
+  'john': {
+    'age': 21,
+    'hobby': 'fishing'
+  },
+  'mike': {
+    'age': 22,
+    'hobby': 'singing'
+  }
+}
+
+print(boys)
