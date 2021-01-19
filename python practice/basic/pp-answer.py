f68e3e90c285a5a28a8c9de8f0393d64c7592290
sd1104 <67669319+sd1104@users.noreply.github.com>
2021-01-19 23:24:13 +0900

    Update pp-answer.py

--- python practice/basic/pp-answer.py
+++ python practice/basic/pp-answer.py
@@ -2,49 +2,53 @@ cd python\ practice/basic
 
 
 fruits = ['apple', 'orange', 'grape']
-fruits.insert(0, 'peach')
-fruits.insert(1, 'melon')
-fruits.append('strawberry')
-fruits.extend(['pine', 'lemon'])
 
-boys = {
-  'john': {
-    'age': 20,
-    'hobby': 'fishing',
-    'job': 'hunter'
-  },
-  'mike': {
-    'age': 21,
-    'hobby': 'singing',
-    'job': 'assasin'
-  }
-}
+fruits.shift(1)
 
-for index, value in enumerate(fruits):
-  print(index, value)
+print(fruits)
+# fruits.insert(0, 'peach')
+# fruits.insert(1, 'melon')
+# fruits.append('strawberry')
+# fruits.extend(['pine', 'lemon'])
 
-for i in boys:
-  for f in boys[i]:
-    print(i, f, boys[i][f])
+# boys = {
+#   'john': {
+#     'age': 20,
+#     'hobby': 'fishing',
+#     'job': 'hunter'
+#   },
+#   'mike': {
+#     'age': 21,
+#     'hobby': 'singing',
+#     'job': 'assasin'
+#   }
+# }
 
-import random
+# for index, value in enumerate(fruits):
+#   print(index, value)
 
-def make_plus():
-  i = 0
-  while i < 10:
-    numA = random.randint(0,10)
-    numB = random.randint(0,10)
-    plus = numA + numB
-    formula = '{} + {}'
-    formula1 = formula.format(numA, numB)
-    print(formula1)
-    i += 1
+# for i in boys:
+#   for f in boys[i]:
+#     print(i, f, boys[i][f])
 
-height = 101
+# import random
 
-if height == 180:
-  print(180)
-elif height == 100:
-  print(100)
-else:
-  print('other')
+# def make_plus():
+#   i = 0
+#   while i < 10:
+#     numA = random.randint(0,10)
+#     numB = random.randint(0,10)
+#     plus = numA + numB
+#     formula = '{} + {}'
+#     formula1 = formula.format(numA, numB)
+#     print(formula1)
+#     i += 1
+
+# height = 101
+
+# if height == 180:
+#   print(180)
+# elif height == 100:
+#   print(100)
+# else:
+#   print('other')
